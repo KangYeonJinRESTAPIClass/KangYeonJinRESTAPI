f6c8c29034a1b517434d743a56f83da1c3a5d5a2
kangyeonjin <yeonjin917n.n@gmail.com>
2024-09-04 17:45:52 +0900

    수정

--- src/main/java/com/ohgiraffers/mission/service/PostService.java
+++ src/main/java/com/ohgiraffers/mission/service/PostService.java
@@ -3,12 +3,14 @@ package com.ohgiraffers.mission.service;
 import com.ohgiraffers.mission.domain.dto.PostDTO;
 import com.ohgiraffers.mission.domain.entity.Post;
 import com.ohgiraffers.mission.repository.PostRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,10 +45,30 @@ public class PostService {
     }
 
 
+    public void updatePost(int postNo, PostDTO modifyInfo) {
 
+        // postNo에 해당하는 게시글을 찾음
+        Optional<Post> postOptional = postRepository.findById(postNo);
 
+        if (postOptional.isPresent()) {
+            Post existingPost = postOptional.get();
+            // 게시글 내용을 수정함
+            existingPost.setTitle(modifyInfo.getTitle());
+            existingPost.setContent(modifyInfo.getContent());
 
+            // 수정된 게시글을 저장함
+            postRepository.save(existingPost);
 
+        } else {
+            // postNo에 해당하는 게시글이 없을 경우 예외를 던질 수도 있음
+            throw new EntityNotFoundException("Post not found with ID: " + postNo);
+        }
 
+    }
 
+    public Optional<PostDTO> getPostById(int postNo) {
+
+        Optional<Post> postOptional = postRepository.findById(postNo);
+        return postOptional.map(this::convertToDTO);
+    }
 }
